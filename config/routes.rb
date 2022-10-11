aef0646c41c12a9efcdee03964d85b12e88a456c
vinay-wi <vinay.kp@weinvest.net>
2022-10-11 05:54:18 +0530

    KP : added other http methods for 405

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post "inbound/sms" => "sms#inbound"
   post 'outbound/sms' => "sms#outbound"
-  match "*path", :to => "application#routing_error", via: [:get]
+  match "*path", :to => "application#routing_error", via: [:get, :delete, :patch, :put]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
